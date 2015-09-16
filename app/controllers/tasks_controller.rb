40a11a0274c7b947d94779d44c7949fa13520a64
Nidhin Nambiar <dnidhin5@gmail.com>
2015-09-16 17:35:56 +0530

    index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
   def create
+   current_user.tasks.create(task_params)
+    redirect_to users_path
   end
-
+  private
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
