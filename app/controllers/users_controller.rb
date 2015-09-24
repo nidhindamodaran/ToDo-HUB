bb6a542d7b82b1509ce6dc08143954f18e08cc64
Nidhin Nambiar <dnidhin5@gmail.com>
2015-09-24 12:45:06 +0530

    User show, edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    @user = User.find(params[:id])
+    @tasks = Task.where(user_id:params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
 
 end
